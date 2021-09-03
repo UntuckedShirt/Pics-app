00f2bbb1492a781e359a48b13ad8026a69cd66c8
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-09-03 16:30:30 -0400

    v1 comleted

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,19 @@
 // To communicate list of images to the imageList
 // we do that using a prop system
 // use images={this.state.images} in the app file
-
+// how to fix this error; Each child in a list should have a unique "key" prop.
+// the list of items are being rendered through images child and ImageList parent
+// image is mentioned multi times. we can refactor and condense that
+// find image arguement. then delete it and put () and {}
 
 import React from 'react'
 
-const ImageList = (props) => {
-    console.log(props.images)
-    return <div> ImageList</div>;
+const ImageList = props => {
+    const images = props.images.map(({description, id, urls}) => {
+        return <img alt={description} key={id} src={urls.regular} />
+    });
+
+    return <div>{images}</div>;
 
 }
 
