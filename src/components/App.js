4e97acf007c11d846a87113dd79b56b593bef1ba
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-09-04 01:46:30 -0400

    image app finale

--- src/components/App.js
+++ src/components/App.js
@@ -29,27 +29,26 @@ import ImageList from './ImageList'
 // async await is new and shorter
 // ---------------------------------------------------------------//
 
-class App extends React.Component { 
-    state = { images: [] }
-        onSeachSubmit = async (term) => {
-            const response = await unsplash.get('/search/photos', {
-            params: { query: term },
-            
-       
-       });
-        this.setState({ images: response.data.results });
-    }
-
-
+class App extends React.Component {
+    state = { images: [] };
+  
+    onSearchSubmit = async term => {
+      const response = await unsplash.get('/search/photos', {
+        params: { query: term }
+      });
+  
+      this.setState({ images: response.data.results });
+    };
+  
     render() {
-        return (
-            <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSeachSubmit} />
-                <ImageList images={this.state.images}/>
-            </div>
-        );
+      return (
+        <div className="ui container" style={{ marginTop: '10px' }}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+          <ImageList images={this.state.images} />
+        </div>
+      );
     }
-    
-}
-
-export default App
+  }
+  
+  export default App;
+  
